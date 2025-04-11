cd221fa605812ab7d09212c63867cd5fdf410c60
ananyagoyal1001 <145464347+ananyagoyal1001@users.noreply.github.com>
2025-04-11 16:01:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 
-remedies = st.Page("working_page/Remedies.py", title="Remedies")
-Consumer = st.Page("working_page/UK Consumer law automated.py", title="Consumer Law")
-Legal = st.Page("working_page/legal.py", title="Legal page")
+remedies = st.Page("pages/Remedies.py", title="Remedies")
+Consumer = st.Page("pages/UK Consumer law automated.py", title="Consumer Law")
+Legal = st.Page("pages/legal.py", title="Legal page")
 
 # Organize dibs under the respective categories, including icons
 pg = st.navigation(
