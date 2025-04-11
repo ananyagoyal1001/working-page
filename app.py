316db7ea5f65b613355714fce663f19974dadc4d
ananyagoyal1001 <145464347+ananyagoyal1001@users.noreply.github.com>
2025-04-11 16:03:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import numpy as np
 
 remedies = st.Page("pages/Remedies.py", title="Remedies")
 Consumer = st.Page("pages/UK Consumer law automated.py", title="Consumer Law")
-Legal = st.Page("pages/legal.py", title="Legal page")
 
 # Organize dibs under the respective categories, including icons
 pg = st.navigation(
     {
-        "🥐 France": [remedies], "UK": [Consumer, Legal]
+        "🥐 France": [remedies], "UK": [Consumer]
     }
 )
 
